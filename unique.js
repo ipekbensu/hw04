724a17aae422d7501ce618d2ac1faa252f74c985
ipekbensu <ibmanav@gmail.com>
2019-03-12 16:44:40 -0400

    added unique.js

---
+++ unique.js
@@ -0,0 +1,27 @@
+// Homework 4: Data mapping for MBTA Route 1 buses
+// repo: https://github.com/ipekbensu/hw04
+
+// Part 3: Count unique IDs
+
+// NPM lowdb
+// doc: https://www.npmjs.com/package/lowdb
+
+var low = require('lowdb');
+var FileSync = require('lowdb/adapters/FileSync');
+var adapter = new FileSync('db.json');
+var db = low(adapter);
+
+db.defaults({vehicles: []}) // init store
+    .write();
+
+// list IDs
+var IDs = db.get('vehicles').map('id').value();
+// var IDCount = IDs.length;
+// // console.log('IDs: ' + IDs);
+// // console.log('# of IDs: ' + IDCount);
+
+// list unique IDs
+var uniqueIDs = [...new Set(IDs)];
+var uniqueIDCount = uniqueIDs.length;
+console.log('unique IDs: ' + uniqueIDs);
+console.log('# of unique IDs: ' + uniqueIDCount);
