903cfd573dce5a0232cc543ec6ae1fb608cad1f4
ipekbensu <ibmanav@gmail.com>
2019-03-12 15:55:01 -0400

    added count.js

---
+++ count.js
@@ -0,0 +1,21 @@
+// Homework 4: Data mapping for MBTA Route 1 buses
+// repo: https://github.com/ipekbensu/hw04
+
+// Part 2: Count entries
+
+// NPM lowdb
+// doc: https://www.npmjs.com/package/lowdb
+
+var low = require('lowdb');
+var FileSync = require('lowdb/adapters/FileSync');
+var adapter = new FileSync('db.json');
+var db = low(adapter);
+
+db.defaults({vehicles: []}) // init store
+    .write();
+
+// count entries
+var entries = db.get('vehicles').size().value();
+
+// log number
+console.log('# of entries: ' + entries);
